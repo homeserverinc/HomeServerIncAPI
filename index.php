988c05c5f4b6e3648f51eece04adeff71e99c571
marcioelias <marcioelias@gmail.com>
2018-11-28 16:14:41 -0200

    Include more examples of use.

--- index.php
+++ index.php
@@ -8,5 +8,18 @@ $client = new HomeServerApiClient('superadministrator@app.com', 'password');
 
 
 if ($client->auth()) {
-    echo $client->getSite('49c96f2f-733b-468f-b374-bc3f3116cb34');
+    $response = $client->getSite('49c96f2f-733b-468f-b374-bc3f3116cb34');
+
+    //working with arrays
+    echo "<h1>Arrays</h1>";
+    $site = json_decode($response, true)['data'];
+    echo $site['name'].'<br />';
+    echo $site['phone']['friendly_name'].'<br />';
+
+
+    echo "<h1>StdClass</h1>";
+    //working with StdClass
+    $site = json_decode($response)->{'data'};
+    echo $site->{'name'}.'<br />';
+    echo $site->{'phone'}->{'friendly_name'}.'<br />';
 }
